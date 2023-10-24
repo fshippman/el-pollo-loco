dd5e98cc1225d1bd3a112cab5935041450d63ae0
fshippman <fshippman@yahoo.com>
2023-10-24 11:58:23 +0200

    cloud animation

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -6,7 +6,18 @@ class Cloud extends MovableObject {
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
         this.x = Math.random() * 500;
-       
+        this.animate();
     }
 
+    /**
+     * This function animates the clouds
+     * 
+     */
+    animate() {
+        // 60 mal Pro Sekunde werden Pixel um 0,15 verringert
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60);
+
+    }
 }
