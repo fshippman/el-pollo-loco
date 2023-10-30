358b073bc8f8be4bcca9d95568dcc0c564439b6b
fshippman <fshippman@yahoo.com>
2023-10-30 15:05:30 +0100

    new classes status bar and drawable object

---
+++ models/drawable-object.class.js
@@ -0,0 +1,42 @@
+ class DrawableObject {
+    x = 120;
+    y = 280;
+    width = 100;
+    height = 150;
+    offsetY = 0;
+    img;
+    imageCache = {};
+    currentImage = 0;
+    
+    /**
+      * This function
+      * 
+      * @param {string} path -  der Pfad zu unserem Bild 'img/2_character_pepe/2_walk/W-21.png'
+      */
+     // loadImage('img/test.png');
+     loadImage(path) {
+         this.img = new Image(); //this.img = document.getElementById('image') <img id="image">
+         this.img.src = path; // es fehlt noch src
+     }
+
+     draw(ctx) {
+         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+
+     }
+
+     
+     /**
+      * Bilder werden  ins JSON imageCache geladen
+      * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+      * 6 mal durch die schleife durch da wird variable angelegt mit einem neuen Bild (img = new Image)
+      * dann wird das bild ins img objekt geladen
+      * dann wird imageCache geuptdatet
+      */
+     loadImages(arr) {
+         arr.forEach((path) => {
+             let img = new Image();
+             img.src = path;
+             this.imageCache[path] = img;
+         })
+     }
+ }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,12 +1,4 @@
-class MovableObject {
-    x = 120;
-    y = 280;
-    offsetY = 0;
-    height = 150;
-    width = 100;
-    img;
-    imageCache = {};
-    currentImage = 0;
+class MovableObject extends DrawableObject {
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
@@ -23,27 +15,9 @@ class MovableObject {
         }, 1000 / 25);
     }
 
-
     isAboveGround() {
         return this.y < 150
     }
-
-    /**
-     * This function
-     * 
-     * @param {string} path -  der Pfad zu unserem Bild 'img/2_character_pepe/2_walk/W-21.png'
-     */
-    // loadImage('img/test.png');
-    loadImage(path) {
-        this.img = new Image(); //this.img = document.getElementById('image') <img id="image">
-        this.img.src = path; // es fehlt noch src
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-
-    }
-
 
     /**
      * This function draws a frame of movable objects if they are an instance of character or chicken
@@ -92,20 +66,7 @@ class MovableObject {
         return this.energy == 0;
     }
 
-    /**
-     * Bilder werden  ins JSON imageCache geladen
-     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
-     * 6 mal durch die schleife durch da wird variable angelegt mit einem neuen Bild (img = new Image)
-     * dann wird das bild ins img objekt geladen
-     * dann wird imageCache geuptdatet
-     */
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        })
-    }
+    
 
     playAnimation(images) {
         let i = this.currentImage % images.length; // let i = 7 % 6, => 1, Rest 1
