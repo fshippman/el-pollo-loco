6dd0ffb883f150158a186dbd0c2bee9e0d2358fe
fshippman <fshippman@yahoo.com>
2023-10-30 18:19:28 +0100

    statusbar start

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -1,4 +1,5 @@
-class StatusBar{
+class StatusBar extends DrawableObject {
+
     ENERGY_BAR = [
         'assets/img/7_statusbars/1_statusbar/2_statusbar_health/green/0.png',
         'assets/img/7_statusbars/1_statusbar/2_statusbar_health/green/20.png',
@@ -6,10 +7,35 @@ class StatusBar{
         'assets/img/7_statusbars/1_statusbar/2_statusbar_health/green/60.png',
         'assets/img/7_statusbars/1_statusbar/2_statusbar_health/green/80.png',
         'assets/img/7_statusbars/1_statusbar/2_statusbar_health/green/100.png'
-    ]; 
+    ];
 
-    constructor(){
-        this.loadImages(this.ENERGY_BAR);
+    percentage = 100;
+
+    constructor() {
+        super().loadImages(this.ENERGY_BAR);
+    }
+    
+    //setPercentage(50)
+    setPercentage(percentage) {
+        this.percentage = percentage; // => 0 ... 5
+        let path = this.ENERGY_BAR[this.resolveImageIndex()]
+        this.img = this.imageCache[path];
+
+    }
+
+    resolveImageIndex() {
+        if (percentage == 100) {
+            return 5;
+        } else if (percentage > 80) {
+            return 4;
+        } else if (percentage > 60) {
+            return 3;
+        } else if (percentage > 40) {
+            return 2;
+        } else if (percentage > 20) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
-
