2f2e84b973e2940795780c9ba75615f5a2a66739
fshippman <fshippman@yahoo.com>
2023-12-22 20:14:05 +0100

    win lose screen first steps responsive

--- js/game.js
+++ js/game.js
@@ -46,14 +46,14 @@ function stopGameWin() {
         document.getElementById('winningScreen').classList.remove('d-none');
         document.getElementById('loseScreen').classList.add('d-none');
         document.getElementById('startScreen').classList.add('d-none');
-        document.getElementById('canvas').classList.add('d-none');
+        // document.getElementById('canvas').classList.add('d-none');
         document.getElementById('loadingScreen').classList.add('d-none');
     }, 3000);
 }
 
 function stopGameLose() {
     pauseAllSounds();
-    playLosingSound();
+    playLosingSound(); 
     document.getElementById('loseScreen').classList.remove('d-none');
     document.getElementById('winningScreen').classList.add('d-none');
     document.getElementById('startScreen').classList.add('d-none');
