fa0e00c6773a08aa51d11c3b0a0a6f9b5c03121f
fshippman <fshippman@yahoo.com>
2024-01-11 15:53:57 +0100

    clouds js doc

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -5,27 +5,28 @@ class Cloud extends MovableObject {
     height = 250;
     speed = 0.15;
 
+
+    /**
+     * Constructs a new Cloud instance.
+     * Initializes the cloud with an image, sets a random speed for movement, and starts the animation.
+     * The initial position is randomly determined along the x-axis, and a fixed position is set on the y-axis.
+     */
     constructor() {
         super();
         this.loadImage('./assets/img/5_background/layers/4_clouds/1.png');
-        this.speed = 0.15 + Math.random() * 0.15; //0.25
+        this.speed = 0.15 + Math.random() * 0.15;
         this.animate();
     }
 
+
     /**
-     * This function animates the clouds
-     * 
+     * Manages the cloud's continuous movement.
+     * This method sets an interval to move the cloud to the left.
      */
     animate() {
         setInterval(() => {
-            // if (world.gameIsRunning) {
-                this.moveLeft();
-            // }
-           
-           
+            this.moveLeft();
         }, 1000 / 60);
     }
-
-
 
 }
