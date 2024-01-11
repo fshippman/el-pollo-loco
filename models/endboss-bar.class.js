ee22eb5d0dcd05a1b1ac55dbae2eb9b190bcea34
fshippman <fshippman@yahoo.com>
2024-01-11 16:09:25 +0100

    endboss bar

--- models/endboss-bar.class.js
+++ models/endboss-bar.class.js
@@ -1,4 +1,5 @@
 class EndbossBar extends DrawableObject {
+    percentage = 100;
 
     ENDBOSS_ENERGY_BAR = [
         './assets/img/7_statusbars/2_statusbar_endboss/green.png',
@@ -6,8 +7,12 @@ class EndbossBar extends DrawableObject {
         './assets/img/7_statusbars/2_statusbar_endboss/orange.png',
     ];
 
-    percentage = 100;
 
+    /**
+     * Constructs a new EndbossBar instance.
+     * Initializes the endboss energy bar with a series of images representing different energy levels.
+     * Sets the initial position, dimensions, and the starting percentage of the energy bar to full (100%).
+     */
     constructor() {
         super();
         this.loadImages(this.ENDBOSS_ENERGY_BAR);
@@ -18,16 +23,24 @@ class EndbossBar extends DrawableObject {
         this.setPercentage(100);
     }
 
-    //
+
+    /**
+     * Sets the energy level of the endboss bar and updates the displayed image accordingly.
+     * @param {number} percentage - The energy level percentage of the endboss, ranging from 0 to 100.
+     */
     setPercentage(percentage) {
-        this.percentage = percentage; // => 0 ... 100
-        let path = this.ENDBOSS_ENERGY_BAR[this.resolveBossBarImageIndex(percentage)] // => 0 ... 5  => richtiges Bild aus dem ENERGY_BAR Array wird path zugewiesen
+        this.percentage = percentage;
+        let path = this.ENDBOSS_ENERGY_BAR[this.resolveBossBarImageIndex(percentage)];
         this.img = this.imageCache[path];
     }
 
-// 85 70  55 40  25 10
 
-    // gibt Wert zwischen 0 und 2 zurück basierend auf Prozentwerd weil 3 Balkenbilder vorhanden
+    /**
+     * Resolves the index of an image for the endboss bar based on the current energy level percentage.
+     * This method is used to select the appropriate energy bar image reflecting the endboss's current state.
+     * @param {number} percentage - The current energy level percentage of the endboss.
+     * @returns {number} The index of the corresponding energy bar image.
+     */
     resolveBossBarImageIndex(percentage) {
         if (percentage > 65) {
             return 0;
@@ -37,4 +50,5 @@ class EndbossBar extends DrawableObject {
             return 2;
         }
     }
+    
 }
