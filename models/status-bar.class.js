8b6f713e97677f21b8f6b55a2635828a109dee18
fshippman <fshippman@yahoo.com>
2024-01-17 11:30:12 +0100

    status bar js doc

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -11,22 +11,30 @@ class StatusBar extends DrawableObject {
 
     percentage = 100;
 
+
+    /**
+     * Constructs a new StatusBar instance.
+     * Initializes the status bar with a series of images representing different fill levels.
+     * Sets the initial position, dimensions, and the starting percentage of the status bar to full (100%).
+     */
     constructor() {
         super();
         this.loadImages(this.ENERGY_BAR);
         this.x = 20; //50
         this.y = 0;
         this.width = 200;
-        this.height = 60 ;
+        this.height = 60;
         this.setPercentage(100);
     }
 
-    //
+
+    /**
+     * Sets the fill percentage of the status bar and updates the displayed image accordingly.
+     * @param {number} percentage - The percentage to set for the status bar, ranging from 0 to 100.
+     */
     setPercentage(percentage) {
-        this.percentage = percentage; // => 0 ... 100
-        let path = this.ENERGY_BAR[this.resolveImageIndex(percentage)] // => 0 ... 5  => richtiges Bild aus dem ENERGY_BAR Array wird path zugewiesen
+        this.percentage = percentage; 
+        let path = this.ENERGY_BAR[this.resolveImageIndex(percentage)]
         this.img = this.imageCache[path];
     }
 }
-
-
